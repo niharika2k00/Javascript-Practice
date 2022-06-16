d0755ae8b116eb6584962a0e75d4b36959029175
niharika2k00 <dniharika16@gmail.com>
2022-06-16 22:11:09 +0530

    js

---
+++ Js-DS/Snippet.js
@@ -0,0 +1,43 @@
+//   -------    SNIPPET - 1   -----------
+function a() {
+  for (let i = 0; i < 3; i++) {
+    function fun() {
+      i++;
+      setTimeout(() => {
+        console.log(i--);
+      }, 400);
+    }
+
+    fun(i);
+  }
+}
+
+a();
+
+//  Output : 1 3
+
+//   -------    SNIPPET - 2   -----------
+let i = 0;
+function fun() {
+  i++;
+  setTimeout(() => {
+    console.log(i--);
+  }, 400);
+}
+
+for (let i = 0; i < 3; i++) {
+  fun(i);
+}
+
+//   -------    SNIPPET - 3   -----------
+let i = 0;
+function fun(i) {
+  i++;
+  setTimeout(() => {
+    console.log(i--);
+  }, 400);
+}
+
+for (let i = 0; i < 3; i++) {
+  fun(i);
+}
--- index1.js
+++ index1.js
@@ -42,9 +42,56 @@ var res = z();
 console.log("Result from the func call " + res); */
 
 // In JavaScript, all objects interact by REFERENCE when setting them equal to each other.
-let c = { greeting: "Hey!" };
+/* let c = { greeting: "Hey!" };
 let d;
 
 d = c;
 c.greeting = "Hello";
 console.log(d.greeting); // Hello
+ */
+
+function x() {
+  var a = 200,
+    b = 500;
+  function y() {
+    console.log("Value coming due to Lexical Environment : ", a);
+  }
+
+  // y();
+  return y;
+}
+
+var closure = x();
+// console.log(closure);
+closure();
+
+// x()(); // calling the inenr func
+
+let i = 0;
+function fun() {
+  i++;
+  setTimeout(() => {
+    console.log(i--);
+  }, 400);
+}
+
+for (let i = 0; i < 3; i++) {
+  fun(i);
+}
+
+let i = 0;
+
+function a() {
+  for (let i = 0; i < 3; i++) {
+    function fun() {
+      i++;
+      setTimeout(() => {
+        console.log(i--);
+      }, 400);
+    }
+
+    fun(i);
+  }
+}
+
+a();
